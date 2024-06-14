8fb2ecff32b0ee1e0fd53129941d426567c17254
Piotr Stachaczynski <piotr.stachaczynski@priva.com>
2024-06-14 22:27:37 +0200

    feat: init version

---
+++ Frontend/MainFE/Program.cs
@@ -0,0 +1,31 @@
+using MainFE;
+using MainFE.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddFluentUIComponents();
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<ModelSelectionService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ MaIN/TempDb.cs
@@ -0,0 +1,6 @@
+namespace MaIN;
+
+public static class TempDb
+{
+    public static Dictionary<string,ChatDto> Chats = [];
+}
