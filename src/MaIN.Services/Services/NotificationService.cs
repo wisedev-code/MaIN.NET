639e8964aacb14ac9e8cd172bd2b89292ab64fb7
Piotr Stachaczynski <piotr.stachaczynski@priva.com>
2024-10-08 22:57:20 +0200

    quickie

--- src/MaIN.Services/Services/NotificationService.cs
+++ src/MaIN.Services/Services/NotificationService.cs
@@ -11,7 +11,7 @@ public class NotificationService : INotificationService
         var msg = JsonSerializer.Serialize(message);
         var originalColor = Console.ForegroundColor;
         Console.ForegroundColor = ConsoleColor.Blue;
-        Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {}");
+        Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {msg}");
         Console.ForegroundColor = originalColor;
 
         return Task.CompletedTask;
