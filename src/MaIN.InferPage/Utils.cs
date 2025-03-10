bcb5765c653491637e68438a5f016b8b1622a72d
Piotr Stachaczynski <piotr.stachaczynski@priva.com>
2025-03-10 22:50:30 +0100

    feat: cd infer command

--- src/MaIN.InferPage/Utils.cs
+++ src/MaIN.InferPage/Utils.cs
@@ -7,6 +7,8 @@ public static class Utils
     public static string Model = "gemma2:2b";
     public static bool Visual => VisualModels.Contains(Model);
     private static string[] VisualModels = ["FLUX.1_Shnell", "dall-e-3"];
+    public static bool OpenAi { get; set; }
+    public static string Path { get; set; }
 }
 
 public static class Extensions
---
+++ src/MaIN.InferPage/wwwroot/scroll.js
@@ -0,0 +1,3 @@
+window.scrollToBottom = (element) => {
+    element.scrollIntoView({ behavior: 'smooth' });
+};
