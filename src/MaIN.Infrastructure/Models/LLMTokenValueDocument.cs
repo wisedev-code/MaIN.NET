e021c93e910675cc1b6beead8be25efa4c55ac1b
Piotr Stachaczynski <piotr.stachaczynski@priva.com>
2025-03-26 08:14:42 +0100

    feat: cleanup markups

--- src/MaIN.Infrastructure/Models/LLMTokenValueDocument.cs
+++ src/MaIN.Infrastructure/Models/LLMTokenValueDocument.cs
@@ -5,5 +5,5 @@ namespace MaIN.Infrastructure.Models;
 public class LLMTokenValueDocument
 {
     public required string Text { get; set; }
-    public TokenType Type { get; set; }
+    public TokenType Type { get; set; } //TODO add document representation of this domain enum
 }
