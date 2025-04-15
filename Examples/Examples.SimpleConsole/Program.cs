b5a4ef0c6fc3a1c1528d4edb800741e6bde5f1a4
Piotr Stachaczynski <piotr.stachaczynski@priva.com>
2025-04-15 09:15:10 +0200

    cleanup usings

--- Examples/Examples.SimpleConsole/Program.cs
+++ Examples/Examples.SimpleConsole/Program.cs
@@ -1,9 +1,5 @@
 ﻿using MaIN.Core;
 using MaIN.Core.Hub;
-using MaIN.Core.Hub.Utils;
-using MaIN.Domain.Entities;
-using MaIN.Domain.Entities.Agents.AgentSource;
-using MaIN.Services.Services.Models.Commands;
 
 MaINBootstrapper.Initialize();
 
