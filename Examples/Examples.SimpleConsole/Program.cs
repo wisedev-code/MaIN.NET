99a0620c8a0ce68dcaecf76d15a7fab51772c61b
Piotr Stachaczynski <piotr.stachaczynski@priva.com>
2025-04-21 22:09:25 +0200

    feat: improve KM performance

--- Examples/Examples.SimpleConsole/Program.cs
+++ Examples/Examples.SimpleConsole/Program.cs
@@ -1,10 +1,25 @@
 ﻿using MaIN.Core;
 using MaIN.Core.Hub;
+using MaIN.Domain.Entities;
 
 MaINBootstrapper.Initialize();
 
-await AIHub.Chat()
-    .WithModel("gemma2:2b")
-    .WithMessage("Hello, World!")
-    .CompleteAsync(interactive: true);
+// await AIHub.Chat()
+//     .WithModel("gemma2:2b")
+//     .WithMessage("Hello, World!")
+//     .CompleteAsync(interactive: true);
+
+var result = AIHub.Chat()
+    .WithModel("llama3.2:3b")
+    .WithMessage(
+        "Write this invoice as JSON.")
+    .WithMemoryParams(new MemoryParams()
+    {
+        AnswerTokens = 2000,
+        ContextSize = 4500,
+    })
+    .WithFiles(["3.pdf"], preProcess: true);
+    
+var chatResult = await result.CompleteAsync();
+Console.WriteLine(chatResult.Message.Content);
 
