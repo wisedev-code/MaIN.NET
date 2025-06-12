61cbf1b00e701ce22070b4e4aa182faa308789e8
Piotr Stachaczynski <piotr.stachaczynski@priva.com>
2025-06-12 13:12:14 +0200

    feat: aihub contains needed stuff

--- src/MaIN.Core/Hub/AiHub.cs
+++ src/MaIN.Core/Hub/AiHub.cs
@@ -1,6 +1,7 @@
 using LLama.Native;
 using MaIN.Core.Hub.Contexts;
 using MaIN.Core.Interfaces;
+using MaIN.Domain.Configuration;
 using MaIN.Services.Services.Abstract;
 
 namespace MaIN.Core.Hub;
@@ -8,10 +9,16 @@ namespace MaIN.Core.Hub;
 public static class AIHub
 {
     private static IAIHubServices? _services;
+    private static MaINSettings _settings;
+    private static IHttpClientFactory _httpClientFactory;
 
-    internal static void Initialize(IAIHubServices services)
+    internal static void Initialize(IAIHubServices services,
+        MaINSettings settings, 
+        IHttpClientFactory httpClientFactory)
     {
         _services = services;
+        _settings = settings;
+        _httpClientFactory = httpClientFactory;
     }
 
     private static IAIHubServices Services =>
@@ -19,6 +26,7 @@ public static class AIHub
         throw new InvalidOperationException(
             "AIHub has not been initialized. Make sure to call AddAIHub() in your service configuration.");
 
+    public static ModelContext Model() => new ModelContext(Services.)
     public static ChatContext Chat() => new(Services.ChatService);
     public static AgentContext Agent() => new(Services.AgentService);
     public static FlowContext Flow() => new(Services.FlowService, Services.AgentService);
