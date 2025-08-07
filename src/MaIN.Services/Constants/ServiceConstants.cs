8d61e5026623f776c1d1a9953d8e20fe99427221
Magdalena Majta <67785133+Madzionator@users.noreply.github.com>
2025-08-07 15:13:11 +0200

    GroqCloud integration (#75)
    GroqCloud integration has been added

--- src/MaIN.Services/Constants/ServiceConstants.cs
+++ src/MaIN.Services/Constants/ServiceConstants.cs
@@ -9,6 +9,7 @@ public static class ServiceConstants
         public const string OpenAiClient = "OpenAiClient";
         public const string GeminiClient = "GeminiClient";
         public const string DeepSeekClient = "DeepSeekClient";
+        public const string GroqCloudClient = "GroqCloudClient";
         public const string ImageDownloadClient = "ImageDownloadClient";
         public const string ModelContextDownloadClient = "ModelContextDownloadClient";
     }
@@ -24,7 +25,10 @@ public static class ServiceConstants
         public const string GeminiModels = "https://generativelanguage.googleapis.com/v1beta/models";
 
         public const string DeepSeekOpenAiChatCompletions = "https://api.deepseek.com/v1/chat/completions";
-        public const string DeepSeekModels = "https://api.deepseek.com/models";
+        public const string DeepSeekModels = "https://api.deepseek.com/models";  
+        
+        public const string GroqCloudOpenAiChatCompletions = "https://api.groq.com/openai/v1/chat/completions";
+        public const string GroqCloudModels = "https://api.groq.com/openai/v1/models";
     }
 
     public static class Messages
