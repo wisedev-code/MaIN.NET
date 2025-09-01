e13ece574375dc94dc1bf8b9c11c1f3149b2933b
Piotr Stachaczynski <piotr.stachaczynski@priva.com>
2025-09-01 08:29:52 +0200

    feat: try semantic kernel instead of KM

--- src/MaIN.Services/Services/LLMService/Memory/IMemoryFactory.cs
+++ src/MaIN.Services/Services/LLMService/Memory/IMemoryFactory.cs
@@ -2,12 +2,13 @@ using LLama;
 using LLamaSharp.KernelMemory;
 using MaIN.Domain.Entities;
 using Microsoft.KernelMemory;
+using Microsoft.SemanticKernel.Memory;
 
 namespace MaIN.Services.Services.LLMService.Memory;
 
 public interface IMemoryFactory
 {
-    (IKernelMemory KM, LLamaContext TextGenerationContext, LLamaSharpTextEmbeddingOwn EmbeddingGenerator)
+    ISemanticTextMemory
         CreateMemoryWithModel(string modelsPath, LLamaWeights llmModel, string modelName,
             MemoryParams memoryParams);
     IKernelMemory CreateMemoryWithOpenAi(string openAiKey, MemoryParams memoryParams);
