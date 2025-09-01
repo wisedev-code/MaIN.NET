6f59a29698906d26ead24a3b507227546e79be34
Piotr Stachaczynski <piotr.stachaczynski@priva.com>
2025-09-01 08:39:56 +0200

    feat: test

--- src/MaIN.Services/Services/LLMService/Memory/IMemoryFactory.cs
+++ src/MaIN.Services/Services/LLMService/Memory/IMemoryFactory.cs
@@ -2,13 +2,12 @@ using LLama;
 using LLamaSharp.KernelMemory;
 using MaIN.Domain.Entities;
 using Microsoft.KernelMemory;
-using Microsoft.SemanticKernel.Memory;
 
 namespace MaIN.Services.Services.LLMService.Memory;
 
 public interface IMemoryFactory
 {
-    ISemanticTextMemory
+    (IKernelMemory KM, LLamaContext TextGenerationContext, LLamaSharpTextEmbeddingOwn EmbeddingGenerator)
         CreateMemoryWithModel(string modelsPath, LLamaWeights llmModel, string modelName,
             MemoryParams memoryParams);
     IKernelMemory CreateMemoryWithOpenAi(string openAiKey, MemoryParams memoryParams);
