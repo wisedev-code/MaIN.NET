ac7fd75fa9e3fa762d12bb9b13abd744b2c434fa
Piotr Stachaczynski <piotr.stachaczynski@priva.com>
2025-10-23 15:13:55 +0200

    process pr comments

--- src/MaIN.Domain/Entities/Tools/ToolsConfiguration.cs
+++ src/MaIN.Domain/Entities/Tools/ToolsConfiguration.cs
@@ -2,11 +2,11 @@ namespace MaIN.Domain.Entities.Tools;
 
 public class ToolsConfiguration
 {
-    public List<ToolDefinition>? Tools { get; set; }
+    public required List<ToolDefinition> Tools { get; set; }
     public string? ToolChoice { get; set; }
     
     public Func<string, Task<string>>? GetExecutor(string functionName)
     {
-        return Tools?.FirstOrDefault(t => t.Function.Name == functionName)?.Execute;
+        return Tools.FirstOrDefault(t => t.Function!.Name == functionName)?.Execute;
     }
 }
