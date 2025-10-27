e560aea5d8fae0342e98f1f2b89da37c0bde98db
Piotr Stachaczynski <piotr.stachaczynski@priva.com>
2025-10-27 12:34:38 +0100

    feat: allow any stream to be used

--- src/MaIN.Services/Services/LLMService/ChatMemoryOptions.cs
+++ src/MaIN.Services/Services/LLMService/ChatMemoryOptions.cs
@@ -4,7 +4,7 @@ public class ChatMemoryOptions
 {
     public Dictionary<string, string> TextData { get; set; } = [];
     public Dictionary<string, string> FilesData { get; set; } = [];
-    public Dictionary<string, FileStream> StreamData { get; set; } = [];
+    public Dictionary<string, Stream> StreamData { get; set; } = [];
     public List<string> WebUrls { get; set; } = [];
     public List<string>? Memory { get; set; }  //Is this obsolete? TBD validate how its really used
     public bool PreProcess { get; set; } = false;
