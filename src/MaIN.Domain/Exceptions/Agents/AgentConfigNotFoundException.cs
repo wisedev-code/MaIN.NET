883e85eea3bb705ee59d9ae214fe587fb54f39f9
srebrek <zlotekmikolaj@gmail.com>
2026-03-17 23:00:35 +0100

    Fix invalid message in Agent Config Exception

--- src/MaIN.Domain/Exceptions/Agents/AgentConfigNotFoundException.cs
+++ src/MaIN.Domain/Exceptions/Agents/AgentConfigNotFoundException.cs
@@ -2,8 +2,8 @@ using System.Net;
 
 namespace MaIN.Domain.Exceptions.Agents;
 
-public class AgentConfigNotFoundException(string agentId) : MaINCustomException($"Context of agent with id: '{agentId}' not found.")
+public class AgentConfigNotFoundException(string agentId) : MaINCustomException($"Config of the agent with id: '{agentId}' not found.")
 {
-    public override string PublicErrorMessage => "Agent context not found.";
+    public override string PublicErrorMessage => "Agent config not found.";
     public override HttpStatusCode HttpStatusCode => HttpStatusCode.NotFound;
 }
