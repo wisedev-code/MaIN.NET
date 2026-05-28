4b6944739517e9825f0cf802b1dfecec261a320a
Piotr Stachaczynski <piotr.stachaczynski@priva.com>
2026-05-28 14:37:03 +0200

    fix dockerbuild for interpage

--- src/MaIN.InferPage/wwwroot/load-blazor.js
+++ src/MaIN.InferPage/wwwroot/load-blazor.js
@@ -1,8 +1,15 @@
-Blazor.start({
-    reconnectionOptions: {
-        maxRetries: 10,
-        retryIntervalMilliseconds: function (previousAttempts, maxRetries) {
-            return previousAttempts >= maxRetries ? null : previousAttempts * 1000 + 1000;
-        }
-    }
-});
+(function () {
+    var s = document.createElement('script');
+    s.src = '/_framework/blazor.web.js';
+    s.onload = function () {
+        Blazor.start({
+            reconnectionOptions: {
+                maxRetries: 10,
+                retryIntervalMilliseconds: function (previousAttempts, maxRetries) {
+                    return previousAttempts >= maxRetries ? null : previousAttempts * 1000 + 1000;
+                }
+            }
+        });
+    };
+    document.head.appendChild(s);
+})();
